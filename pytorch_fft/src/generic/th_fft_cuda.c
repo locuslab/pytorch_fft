0e174fc9f4e243d5128ac738d7dac465b52bb1b1
Eric Wong <ericwong@cs.cmu.edu>
2017-05-24 02:17:12 -0400

    removed _ext folder from repo, modified for batch fft

--- pytorch_fft/src/generic/th_fft_cuda.c
+++ pytorch_fft/src/generic/th_fft_cuda.c
@@ -4,91 +4,84 @@
 
 int th_(fft2)(THCTensor *input, THCTensor *output1, THCTensor *output2)
 {
-  printf("Starting fft\n");
-  // THCudaStorage_data(); 
+  // Require that all tensors be of the same size. 
   if (!THCTensor_(isSameSizeAs)(state, input, output1))
     return 0;
   if (!THCTensor_(isSameSizeAs)(state, input, output2))
     return 0;
 
-  int n = THCTensor_(size)(state, input, 0);
+  // Get the tensor dimensions (batchsize, rows, cols). 
+  int batch = THCTensor_(size)(state, input, 0);
   int r = THCTensor_(size)(state, input, 1);
   int c = THCTensor_(size)(state, input, 2);
 
+
+  // Get actual tensor data.
   real *input_data = THCTensor_(data)(state, input);
   real *output1_data = THCTensor_(data)(state, output1);
   real *output2_data = THCTensor_(data)(state, output2);
 
+  // Turn input into a complex array with zero imaginary part. 
   cufft_complex *input_complex; 
-  cudaMalloc((void**)&input_complex, sizeof(cufft_complex)*r*c);
-  cudaMemset(input_complex, 0, sizeof(cufft_complex)*r*c);
+  cudaMalloc((void**)&input_complex, sizeof(cufft_complex)*batch*r*c);
+  if (cudaGetLastError() != cudaSuccess) {
+    fprintf(stderr, "Cuda error: Failed to allocate\n");
+    return -1;
+  }
+  
+  cudaMemset(input_complex, 0, sizeof(cufft_complex)*batch*r*c);
 
   real *input_tmp = (real*)input_complex;
   cudaMemcpy2D(input_tmp, 2*sizeof(real), 
                input_data, sizeof(real), 
-               sizeof(real), r*c, cudaMemcpyDeviceToDevice);
+               sizeof(real), batch*r*c, cudaMemcpyDeviceToDevice);
 
-
+  // Allocate the complex array to store the output
   cufft_complex *output_complex; 
-  cudaMalloc((void**)&output_complex, sizeof(cufft_complex)*r*c);
+  cudaMalloc((void**)&output_complex, sizeof(cufft_complex)*batch*r*c);
   if (cudaGetLastError() != cudaSuccess) {
     fprintf(stderr, "Cuda error: Failed to allocate\n");
     return -1;
   }
 
+  // Make the fft plan. 
   cufftHandle plan;
-  if (cufftPlan2d(&plan, r, c, cufft_type) != CUFFT_SUCCESS) {
+  int rank = 2;
+  int n[2] = {r, c};
+  int idist = r*c;
+  int odist = r*c;
+  int inembed[] = {r, c};
+  int onembed[] = {r, c};
+  int istride = 1;
+  int ostride = 1; 
+  if (cufftPlanMany(&plan, rank, n, 
+                    inembed, istride, idist, 
+                    onembed, ostride, odist, 
+                    cufft_type, batch) != CUFFT_SUCCESS) {
     fprintf(stderr, "CUFFT error: Plan creation failed");
     return -1;
   }
+
+  // Execute the fft plan. 
   if (cufft_exec(plan, input_complex, output_complex, CUFFT_FORWARD) != CUFFT_SUCCESS) {
     fprintf(stderr, "CUFFT error: ExecC2C Forward failed");
     return -1;
   }
-
+  // Not sure if this is necessary. 
   if (cudaThreadSynchronize() != cudaSuccess){
     fprintf(stderr, "Cuda error: Failed to synchronize\n");
     return -1;
   }
 
-  // Reference for working copy
-  // cudaMemcpy(output1_data, input_data, 5*sizeof(input_data[0]), cudaMemcpyDeviceToDevice);
-  // printf("first element pitch: %d, %d\n", sizeof(float), sizeof(real));
-  // cudaMemcpy2D(output2_data, sizeof(real), 
-  //            input_data, sizeof(real), 
-  //            sizeof(real), n*r*c, cudaMemcpyDeviceToDevice);
-
+  // Copy the real and imaginary parts to the output pointers
   real *output_tmp = (real*)output_complex;
   cudaMemcpy2D(output1_data, sizeof(real), 
-             output_tmp, 2*sizeof(real), 
-             sizeof(real), n*r*c, cudaMemcpyDeviceToDevice);
+               output_tmp, 2*sizeof(real), 
+               sizeof(real), batch*r*c, cudaMemcpyDeviceToDevice);
 
   cudaMemcpy2D(output2_data, sizeof(real), 
              output_tmp+1, 2*sizeof(real), 
-             sizeof(real), n*r*c, cudaMemcpyDeviceToDevice);
-
-  // size_t t = sizeof(real)*n*r*c;
-  // real *cpu = malloc(t);
-
-  // cudaMemcpy(cpu, output_tmp, t, cudaMemcpyDeviceToHost);
-  // int i, j;
-  // for(i=0; i<r; i++) {
-  //   for(j=0; j<c; j++){
-  //     printf("%.4f, ", cpu[i*c + j]);
-  //   }
-  //   printf("\n");
-  // }
-  // complex *cpu = malloc(sizeof(complex)*n*r*c);
-
-  // cudaMemcpy(cpu, output_complex, sizeof(complex), cudaMemcpyDeviceToHost);
-  // int i, j;
-  // for(i=0; i<r; i++) {
-  //   for(j=0; j<c; j++){
-  //     printf("%.2f %+.2fi, ", creal(cpu[i*c + j]), cimag(cpu[i*c + j]));
-  //   }
-  //   printf("\n");
-  // }
-  // free(cpu);
+             sizeof(real), batch*r*c, cudaMemcpyDeviceToDevice);
 
   cufftDestroy(plan);
   cudaFree(input_complex);
--- pytorch_fft/test.py
+++ pytorch_fft/test.py
@@ -4,20 +4,27 @@ torch.manual_seed(0)
 import functions.fft as cfft
 import numpy as np
 import numpy.fft as nfft
-n = 5
-x = torch.randn(n*n).view(1, n, n).double().cuda()
 
-print(x)
-print("*"*80)
-if torch.cuda.is_available():
-    y1, y2 = cfft.fft2(x)
-    x_np = x.cpu().numpy().squeeze()
-    y_np = nfft.fft2(x_np)
-    print(y_np.real)
-    print(y1)
-    print(y_np.imag)
-    print(y2)
-    print(np.abs((y1.cpu().numpy()- y_np.real)))
-    print(np.abs((y2.cpu().numpy()- y_np.imag)))
+def run_fft(x):
+    if torch.cuda.is_available():
+        y1, y2 = cfft.fft2(x)
+        x_np = x.cpu().numpy().squeeze()
+        y_np = np.zeros(tuple(x.size()), dtype=np.complex)
+        for i in range(x.size(0)): 
+            y_np[i] = nfft.fft2(x_np[i])
 
+        assert np.allclose(y1.cpu().numpy(), y_np.real)
+        assert np.allclose(y2.cpu().numpy(), y_np.imag)
+    else:
+        print("Cuda not available, cannot test.")
 
+def test_acc(): 
+    batch = 3
+    n = 5
+    m = 7
+    x = torch.randn(batch*n*m).view(batch, n, m).cuda()
+    run_fft(x)
+    run_fft(x.double())
+
+if __name__ == "__main__": 
+    test_acc()
