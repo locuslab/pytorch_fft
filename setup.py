3c85caef47c239d65f9fc86662a1bf207cdd5828
Eric Wong <ericwong@cs.cmu.edu>
2018-04-21 17:07:11 -0400

    bump version to push Rfft to pypi

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ this_file = os.path.dirname(__file__)
 
 setup(
     name="pytorch_fft",
-    version="0.14",
+    version="0.15",
     description="A PyTorch wrapper for CUDA FFTs",
     url="https://github.com/locuslab/pytorch_fft",
     author="Eric Wong",
